b55c0dd1894eec37bbe414fd72d3fefd5f578457
Dhairya Bhardwaj <156906679+DharyaBhardwaj@users.noreply.github.com>
2026-01-28 22:44:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,15 +11,15 @@ API_ID = 22627587
 API_HASH = getenv("API_HASH", "4f664b2a2bf3b095a689a51780a45a5e")
 BOT_TOKEN = getenv("BOT_TOKEN")
 
-OWNER_ID = int(getenv("OWNER_ID", 5968801459))
-OWNER_USERNAME = getenv("OWNER_USERNAME", "Dhairya_bh")
-BOT_USERNAME = getenv("BOT_USERNAME", "Dhairya_music_2bot")
+OWNER_ID = int(getenv("OWNER_ID", 8240512695))
+OWNER_USERNAME = getenv("OWNER_USERNAME", "bhardwa_j")
+BOT_USERNAME = getenv("BOT_USERNAME", "Personalmanagernewbot")
 BOT_NAME = getenv("BOT_NAME", "˹𝑫𝒉𝒂𝒊𝒓𝒚𝒂 𝑿 𝑴𝒖𝒔𝒊𝒄 2˼ ♪")
 ASSUSERNAME = getenv("ASSUSERNAME", "musicxannie")
 
 # ── Database & logging ─────────────────────────────────────────────────────────
 MONGO_DB_URI = getenv("MONGO_DB_URI")
-LOGGER_ID = int(getenv("LOGGER_ID", -1002014167331))
+LOGGER_ID = int(getenv("LOGGER_ID", -5149103823))
 
 # ── Limits (durations in min/sec; sizes in bytes) ──────────────────────────────
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 300))
